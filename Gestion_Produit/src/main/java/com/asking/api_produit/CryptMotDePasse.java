ca5d9dafcd7fa1280f3fd9afdaa2ea68dad8be8d
Charbel Sonon <charbelsnn@gmail.com>
2023-09-27 17:00:46 +0100

    Update

--- Gestion_Produit/src/main/java/com/asking/api_produit/CryptMotDePasse.java
+++ Gestion_Produit/src/main/java/com/asking/api_produit/CryptMotDePasse.java
@@ -1,18 +1,32 @@
 package com.asking.api_produit;
- import org.springframework.context.annotation.Configuration;
+
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
- @Configuration
 
+@Configuration
 public class CryptMotDePasse implements WebMvcConfigurer 
 {
-    
-     public void addViewControllers(ViewControllerRegistry registry) {
+    // Cette classe est une configuration pour gérer les vues et les URLs de l'application.
+
+    // La méthode addViewControllers permet de configurer les associations entre les URLs et les vues.
+    public void addViewControllers(ViewControllerRegistry registry) {
+        // Lorsque l'URL "/home" est accédée, la vue "home" sera affichée.
         registry.addViewController("/home").setViewName("home");
+
+        // L'URL "/" est également associée à la vue "home", donc lorsque l'accès de base est effectué, la vue "home" est affichée.
         registry.addViewController("/").setViewName("home");
+
+        // L'URL "/hello" est associée à la vue "hello".
         registry.addViewController("/hello").setViewName("hello");
+
+        // L'URL "/login" est associée à la vue "Se connecter".
         registry.addViewController("/login").setViewName("Se connecter");
+
+        // L'URL "/" est de nouveau associée à une autre vue "Bienvenue".
         registry.addViewController("/").setViewName("Bienvenue");
+
+        // L'URL "/register" est associée à la vue "S'inscrire".
         registry.addViewController("/register").setViewName("S'inscrire");
     }
 }
--- Gestion_Produit/src/main/java/com/asking/api_produit/modele/Produit.java
+++ Gestion_Produit/src/main/java/com/asking/api_produit/modele/Produit.java
@@ -1,6 +1,7 @@
 package com.asking.api_produit.modele;
- import java.util.Date;
- import javax.persistence.Column;
+
+import java.util.Date;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -11,28 +12,28 @@ import javax.persistence.Transient;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
- import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.multipart.MultipartFile;
- @Entity
+
+@Entity
 @Table(name = "PRODUITS")
 @Getter
 @Setter
 @NoArgsConstructor
 public class Produit {
-     @Id
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-     private String nom;
+    private String nom;
     private double prix;
-     @Column(length = 3)
+    @Column(length = 3)
     private String devise;
-     private Integer taxe;
-     @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Integer taxe;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date dateExpiration;
-     private String fournisseur;
-     private String image; // Nouvelle propriété pour l'image du produit
-     @Transient
-     private MultipartFile imageFile;
-     
+    private String fournisseur;
+    private String image; // Nouvelle propriété pour l'image du produit
+    @Transient
+    private MultipartFile imageFile;
 
- }
+}
